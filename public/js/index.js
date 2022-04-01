611b13f06157b3c77b57e595b5c8cd0730f8141c
Juan Pablo Ubelhart <juanpabloubelhart@hotmail.com>
2022-04-01 01:22:22 -0300

    Desafio WebSockets: Terminado

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,7 @@
 const socket = io.connect();
 
-document.getElementById("button").addEventListener("submit", (e) => {
-  e.preventDefault();
-});
-
-function render(data) {
-  const html = data
+function renderProducts(products) {
+  const html = products
     .map((product) => {
       return `
         <tr>
@@ -18,6 +14,47 @@ function render(data) {
   document.getElementById("table").innerHTML = html;
 }
 
-socket.on("messages", (data) => {
-  render(data);
+socket.on("products", (products) => {
+  renderProducts(products);
 });
+
+document.getElementById("products").addEventListener("submit", (e) => {
+  e.preventDefault();
+});
+
+function addProducts() {
+  const newProduct = {
+    title: document.getElementById("title").value,
+    price: document.getElementById("price").value,
+    thumbnail: document.getElementById("thumbnail").value,
+  };
+  socket.emit("new-products", newProduct);
+}
+
+function renderMessage(messages) {
+  const html = messages
+    .map((elem) => {
+      return `<div>
+        <strong class="email">${elem.email}</strong>
+        <em class="date">[${new Date(Date.now())}]</em>:
+        <em class="text">${elem.text}</em> </div>`;
+    })
+    .join(" ");
+  document.getElementById("messages").innerHTML = html;
+}
+
+socket.on("messages", (messages) => {
+  renderMessage(messages);
+});
+
+document.getElementById("chat").addEventListener("submit", (e) => {
+  e.preventDefault();
+});
+
+function addMessage() {
+  const newMessage = {
+    email: document.getElementById("email").value,
+    text: document.getElementById("text").value,
+  };
+  socket.emit("new-message", newMessage);
+}
--- src/app.js
+++ src/app.js
@@ -1,15 +1,57 @@
 const express = require("express");
 const { Server: HttpServer } = require("http");
 const { Server: IOServer } = require("socket.io");
+const fs = require("fs");
 const server = express();
 const httpServer = new HttpServer(server);
 const io = new IOServer(httpServer);
 const api = require("./script");
 const products = api.products;
 
-io.on("connection", (socket) => {
+async function createIfNotExist() {
+  let messages;
+  try {
+    messages = await fs.promises.readFile("./products.txt");
+  } catch (error) {
+    if (error.code == "ENOENT") {
+      await fs.promises.writeFile("./products.txt", "[]").then(() => {
+        console.log("No existe products.txt. Archivo creado.");
+      });
+      messages = await fs.promises.readFile("./products");
+    } else {
+      console.log("Hubo un error", error);
+    }
+  }
+  return messages;
+}
+
+io.on("connection", async (socket) => {
   console.log("Un cliente se ha conectado");
-  socket.emit("messages", products);
+
+  socket.emit("products", products);
+  socket.on("new-products", (product) => {
+    product.price = parseInt(product.price);
+
+    if (!products.length) {
+      product.id = 1;
+    } else {
+      product.id = products.at(-1).id + 1;
+    }
+    products.push(product);
+    io.sockets.emit("products", products);
+  });
+  const messages = await createIfNotExist();
+  const parsedMessages = JSON.parse(messages);
+
+  socket.emit("messages", parsedMessages);
+  socket.on("new-message", async (data) => {
+    parsedMessages.push(data);
+    io.sockets.emit("messages", parsedMessages);
+    await fs.promises.writeFile(
+      "./products.txt",
+      JSON.stringify(parsedMessages)
+    );
+  });
 });
 
 server.use(express.static("public"));
